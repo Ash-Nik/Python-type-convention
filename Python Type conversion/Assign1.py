b7f5922f64fb61d858ca3d37763c0eee6e20aede
Abhishek Nandi <113675195+Ash-Nik@users.noreply.github.com>
2024-02-28 21:14:20 +0530

    Add files via upload
    its the answers of python type conversion assignment

---
+++ Python Type conversion/Assign1.py
@@ -0,0 +1,48 @@
+# Covert a integer to a floatig-poit umber.
+
+x = 5
+f = float(x)
+print("After converting from int to float = ", f)
+print(type(f))
+print()
+#  Covert a float to a integer.
+
+x = 5.0
+i = int(x)
+print("After converting from float to integer  =", i)
+print(type(i))
+print()
+
+# 3 Covert a integer to a string.
+s = 1234
+s = str(s)
+print("After converting an integer to string : ", s)
+print(type(s))
+print()
+
+#  Covert a list  to a tuple.
+l = [1, 2, 3, 4, 5]
+l = tuple(l)
+print("After converting an list to a tuple = ", l)
+print(type(l))
+print()
+
+#
+# converting a tuple to list
+t = (1, 2, 3, 4, 5)
+t = list(t)
+print("After converting a tuple to a list = ", t)
+print(type(t))
+print()
+
+# Decimal to binary
+d = 10
+d = bin(d)
+print("After converting decimal to binary  = ", d)
+print()
+
+# convert non zero number to boolean
+x=13
+x=bool(x)
+print("After converting a non zero number to boolean = ",x)
+print(type(x))
